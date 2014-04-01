306a3dab867d9a6864d11fc4b075cc7365a58440
Piotr Janik <janikpiotrek@gmail.com>
2014-04-01 10:52:44 +0200

    Make sure that results generation work fine with multiple test threads
    [#67779220]

--- Rakefile
+++ Rakefile
@@ -1,1 +1,7 @@
 require 'sauce'
+
+task :run do
+  ENV['TEST_DIR'] ||= "test_#{Time.now.to_i}"
+  puts "Test directory: #{ENV['TEST_DIR']}"
+  Rake::Task["sauce:spec"].invoke
+end
--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -5,7 +5,7 @@ class TestHelper
   @@expected_screenshots_dir = "#{@@main_dir}/expected_screenshots"
 
   def initialize
-    @test_dir = "#{@@main_dir}/test_#{Time.now.to_i}"
+    @test_dir = "#{@@main_dir}/#{ENV['TEST_DIR']}"
     @images_metadata = []
     # Prepare screenshots/test_<timestamp> folder.
     `mkdir -p #{@test_dir}`
@@ -16,22 +16,33 @@ class TestHelper
     screenshot_path = "#{@test_dir}/#{filename}"
     page.save_screenshot(screenshot_path)
 
-    @images_metadata << {
+    new_image = {
       :filename => filename,
       :diff     => compare_images(screenshot_path, screenshot_path.gsub(@test_dir, @@expected_screenshots_dir)),
       :interactiveUrl => interactive_url
     }
-    generate_js_image_metadata
+    add_js_image_metadata new_image
   end
 
   private 
 
-  def generate_js_image_metadata
-    f = File.new "#{@test_dir}/images_metadata.js", 'w'
-    f.write 'var IMAGES_METADATA = '
-    f.write JSON.pretty_generate(@images_metadata)
-    f.puts  ';'
-    f.close
+  def add_js_image_metadata new_image
+    File.open("#{@test_dir}/images_metadata.js", File::RDWR|File::CREAT, 0644) do |f|
+      begin
+        f.flock File::LOCK_EX
+        content = f.read
+        array = content && content.lines[1..-2] ? JSON.parse(content.lines[1..-2].join) : []
+        array.push new_image
+        f.rewind
+        f.puts 'var IMAGES_METADATA ='
+        f.puts JSON.pretty_generate(array)
+        f.puts ';'
+        f.flush
+        f.truncate f.pos
+      ensure
+        f.flock File::LOCK_UN
+      end
+    end
   end
 
   def compare_images(path_a, path_b) 
