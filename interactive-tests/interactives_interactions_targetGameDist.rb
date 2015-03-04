83b649093b5c277f59b77547d199710cd35400a5
Piotr Janik <janikpiotrek@gmail.com>
2015-03-04 20:07:04 +0100

    Update Lab help icon CSS selector

--- interactive-tests/interactives_interactions_targetGameDist.rb
+++ interactive-tests/interactives_interactions_targetGameDist.rb
@@ -1,6 +1,6 @@
 # Run basic test first.
 load 'interactive-tests/default.rb', true
-$test.click_element('#help-icon')
+$test.click_element('#main-help-icon')
 $test.save_screenshot
 $test.select_radio_option('select-level', 1)
 sleep 1
